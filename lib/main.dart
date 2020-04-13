20de9e70998c48d57f9c15f0f91f7a6bb968c347
athlonneo <athlonneox2@gmail.com>
2020-04-13 08:26:46 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: GoogleMap(
+          initialCameraPosition: CameraPosition(
+            target: const LatLng(-6.200000, 106.816666),
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
+}

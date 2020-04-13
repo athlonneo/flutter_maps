c7bee38b938a37b0befed61ff159f552149703fc
athlonneo <athlonneox2@gmail.com>
2020-04-13 10:00:50 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         body: GoogleMap(
           initialCameraPosition: CameraPosition(
             target: const LatLng(-6.200000, 106.816666),
-            zoom: 11.0,
+            zoom: 1.0,
           ),
         ),
       ),
